6b1980970492163d1bdce62e85e3f709d4fb4baf
nixiong1995 <435712615@qq.com>
2017-12-16 17:32:50 +0800

    12345

--- frontend/controllers/UserController.php
+++ frontend/controllers/UserController.php
@@ -439,6 +439,7 @@ class UserController extends Controller {
                 //接收手机端
                 $user_id=\Yii::$app->request->post('user_id');//用户id
                 $head=isset($_FILES['head'])?$_FILES['head']:'';//头像
+                return $head;
                 $nickname=\Yii::$app->request->post('nickname');//昵称
                 $sex=\Yii::$app->request->post('sex');//性别
                 $birthday=\Yii::$app->request->post('birthday');//生日
