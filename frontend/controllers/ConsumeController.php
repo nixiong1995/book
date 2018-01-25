3c9f862317736d01a65fe0b8924f746a98d1659c
nixiong1995 <435712615@qq.com>
2018-01-25 16:56:20 +0800

    消费计算

--- frontend/controllers/ConsumeController.php
+++ frontend/controllers/ConsumeController.php
@@ -354,12 +354,70 @@ class  ConsumeController extends Controller{
                  ];
                  $post=new PostRequest();
                  $record=json_decode($post->request_post($postUrl,$curlPost));
-                 var_dump($record);exit;
+                 $word_count=$record->content->data->word_count;//该书总字数
+
+                //请求版权方图书章节列表
+                $postUrl2 = 'http://partner.chuangbie.com/partner/chapterlist';
+                $curlPost2 =[
+                    'partner_id'=>2130,
+                    'partner_sign'=>'b42c36ddd1a5cc2c6895744143f77b7b',
+                    'book_id'=>$book->copyright_book_id,
+                ];
+                $post2=new PostRequest();
+                $record2=json_decode($post2->request_post($postUrl2,$curlPost2));
+                $chapter_number=count($record2->content->data);//该书总章节数
+                //var_dump($chapter_number);exit;
+
+                //每章价格
+                $price=round(($word_count/1000*$book->price)/$chapter_number);
                  //查询用户已购章节
                  $purchased=Purchased::find()->where(['user_id'=>$user_id])->one();
+                 $RealPrice=0;//实际价格
+                 $discount=0;//折扣
                  if($purchased){
+                     //已购买该书
+                     $chapter_no=explode('|',$purchased->chapter_no);
+                     //最大章节(也就是最后购买章节)
+                     $chapter_no=array_filter($chapter_no);//去除数组空元素
+                     $no=count($chapter_no);//统计购买章节数量
+                     $new_chapter_no=max($chapter_no);//最新章节号
+                     //剩余购买的章节数
+                     $RemainingChapters=$chapter_number-$no;
+                     $RealPrice=round($RemainingChapters*$price);//实际价格
+
+                     //图书折扣
+                     if($RemainingChapters>=20 && $RemainingChapters<60){//购买20章98折
+                         $SettlementPrice=round($RealPrice*0.98);
+                         $discount=0.98;
+                         //var_dump($price);exit;
+                     }elseif ($RemainingChapters>=60 && $RemainingChapters<100){//购买60章9折
+                         $SettlementPrice=round($RealPrice*0.9);
+                         $discount=0.9;
+                     }elseif ($RemainingChapters>100){//购买100章以上
+                         $SettlementPrice=round($RealPrice*0.8);
+                         $discount=0.8;
+                     }
+
+                 }else{
+                     //没有购买该书
+                     //图书折扣
+                     $RealPrice=$chapter_number*$price;//实际价格
+                     if($chapter_number>=20 && $chapter_number<60){//购买20章98折
+                         $SettlementPrice=round($RealPrice*0.98);
+                         $discount=0.98;
+                         //var_dump($price);exit;
+                     }elseif ($chapter_number>=60 && $chapter_number<100){//购买60章9折
+                         $SettlementPrice=round($RealPrice*0.9);
+                         $discount=0.9;
+                     }elseif ($chapter_number>100){//购买100章以上
+                         $SettlementPrice=round($RealPrice*0.8);
+                         $discount=0.8;
+                     }
 
                  }
+                 $relust['code']=200;
+                 $relust['msg']='获取价格信息成功';
+                 $relust['data']=['RealPrice'=>$RealPrice,'price'=>$price,'discount'=>$discount,'SettlementPrice'=>$SettlementPrice,'ticket'=>$user->ticket,'voucher'=>$user->voucher,'discount'=>$discount,'new_chapter_no'=>isset($new_chapter_no)?$new_chapter_no:''];
 
 
             // }
