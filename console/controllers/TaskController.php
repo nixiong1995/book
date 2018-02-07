c5fbd116152c0e7035986ac52087cacc1fb0e5c7
nixiong1995 <435712615@qq.com>
2018-02-07 17:36:10 +0800

    定时任务自动更新凯兴图书

--- console/controllers/TaskController.php
+++ console/controllers/TaskController.php
@@ -1,6 +1,8 @@
 <?php
 namespace console\controllers;
+use backend\models\Book;
 use backend\models\Recharge;
+use libs\PostRequest;
 use yii\console\Controller;
 
 class TaskController extends Controller{
@@ -18,6 +20,24 @@ class TaskController extends Controller{
             //sleep(1);
             echo '清理完成'.date('Y-m-d H:i:s')."\n";
        // }
+
+    }
+
+    //更新凯兴版权书数据
+    public function actionUpdateCopyright(){
+        $postUrl = 'http://partner.chuangbie.com/partner/booklist';
+        $curlPost =['partner_id'=>2130,'partner_sign'=>'b42c36ddd1a5cc2c6895744143f77b7b','page_size'=>100];
+        $post=new PostRequest();
+        $data=$post->request_post($postUrl,$curlPost);
+        $datas=json_decode($data,true);
+        foreach ($datas['content']['data'] as $data){
+            Book::updateAll(
+                ['size'=>$data['word_count']*2,'is_end'=>$data['status'],'last_update_chapter_id'=>$data['last_update_chapter_id'],'last_update_chapter_name'=>$data['last_update_chapter_name'],'update_time'=>time()],
+                ['name'=>$data['book_name']]);
+
+        }
+
+        echo '批量更新成功'.date('Y-m-d H:i:s')."\n";
 
     }
 }
