aacead06e5d4bf5d4738f2aa9781d78bc1096859
nixiong1995 <435712615@qq.com>
2018-03-14 11:50:46 +0800

    123

--- frontend/controllers/SmallprogramController.php
+++ frontend/controllers/SmallprogramController.php
@@ -34,10 +34,22 @@ class SmallprogramController extends Controller{
                     $relust='未传入指定参数';
                     return $relust;
                 }
-                $book=Book::find()->where(['category_id'=>$category_id])->andWhere(['<>','from',4])->orderBy('update_time DESC')->limit(12)->all();
-                if($book){
+                $books=Book::find()->where(['category_id'=>$category_id])->andWhere(['<>','from',4])->orderBy('update_time DESC')->limit(12)->all();
+                if($books){
+                    foreach ($books as $book){
+                        $relust['data'][]=['book_id'=>$book->id,'name'=>$book->name,
+                            'category'=>$book->category->name,'author'=>$book->author->name,
+                            'view'=>$book->clicks,'image'=>$book->image,'size'=>$book->size,
+                            'score'=>$book->score,'intro'=>$book->intro,'is_end'=>$book->is_end,
+                            'download'=>$book->downloads,'collection'=>$book->collection,'author_id'=>$book->author_id,
+                            'category_id'=>$book->category_id,'no_free'=>$book->no,'type'=>$book->type,
+                            'create_time'=>$book->create_time,'update_time'=>$book->update_time,'from'=>$book->from,
+                            'is_free'=>$book->is_free,'price'=>$book->price,'search'=>$book->search,'sale'=>$book->search,
+                            'ascription_name'=>$book->information->name,'ascription_id'=>$book->ascription,
+                            'copyright_book_id'=>$book->copyright_book_id,'last_update_chapter_id'=>$book->last_update_chapter_id,
+                            'last_update_chapter_name'=>$book->last_update_chapter_name];
+                    }
                     $relust['code']=200;
-                    $relust['data']=$book;
                     $relust['msg']='成功返回图书';
                 }else{
                     $relust['msg']='没有数据';
